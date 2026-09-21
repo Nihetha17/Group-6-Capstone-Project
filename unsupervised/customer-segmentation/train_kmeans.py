0304ffbee98897470eb537eee4dfb5ceb3a0b596
Nihetha17 <nihethanihe@gmail.com>
2026-09-21 21:36:52 +0530

    Updated kmeans

--- unsupervised/customer-segmentation/train_kmeans.py
+++ unsupervised/customer-segmentation/train_kmeans.py
@@ -1,26 +1,61 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-import joblib
+from sklearn.preprocessing import StandardScaler
+import numpy as np 
 
-# Load customer dataset
 customer_data = pd.read_csv("customers.csv")
-
-# Select features
 X = customer_data[["annual_income_k", "spending_score"]]
 
-# Train K-Means model
+# Standard Scaler
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 kmeans = KMeans(n_clusters=3, random_state=42)
 kmeans.fit(X)
 
-# Add cluster labels
 customer_data["Cluster"] = kmeans.labels_
-
-# Print results
 print(customer_data)
-print("\nCluster Centers:")
+print("Cluster Centers:")
 print(kmeans.cluster_centers_)
 
-# Save trained model
-joblib.dump(kmeans, "kmeans_model.pkl")
+plt.scatter(
+    customer_data["annual_income_k"],
+    customer_data["spending_score"],
+    c=customer_data["Cluster"],
+    cmap="viridis")
+plt.scatter(
+    kmeans.cluster_centers_[:, 0],
+    kmeans.cluster_centers_[:, 1],
+    s=200,
+    marker="X",
+    color="red"
+)
 
-print("\nModel trained and saved successfully!")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
+plt.title("Customer Segmentation using K-Means")
+plt.show()
+
+while True:
+    a = int(input("Enter Annual Income (k$): "))
+    b = int(input("Enter Spending Score (1-100): "))
+
+    new_customer = pd.DataFrame(
+        [[a, b]],
+        columns=["annual_income_k", "spending_score"]
+    )
+
+    # Scale new customer
+    new_customer = scaler.transform(new_customer)
+
+    cluster = kmeans.predict(new_customer)
+
+    if cluster[0] == 2:
+        print("UDHARI")
+    elif cluster[0] == 1:
+        print("SELEVALI")
+    else:
+        print("KANJA PISNARI")
+
+    print("Cluster:", cluster[0])
